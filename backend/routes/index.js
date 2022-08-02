34d226ca07d83c81e87c4db78d1fe7caab84fed8
HANA-BOOK\smile <smilecana@gmail.com>
2022-08-02 14:51:08 -0700

    Added account api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const todoRoutes = require('./todo');
 const userRoutes = require('./user');
+const accountRoutes = require('./account')
 
 router.use('/api', todoRoutes);
 router.use('/api', userRoutes);
+router.use('/api/admin', accountRoutes);
 
 
 module.exports = router;
