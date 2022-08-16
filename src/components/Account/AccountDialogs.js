8ab06817eb2abc6f29edc0f4558752114b2961c7
HANA-BOOK\smile <smilecana@gmail.com>
2022-08-16 00:20:35 -0700

    update account features

--- src/components/Account/AccountDialogs.js
+++ src/components/Account/AccountDialogs.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
@@ -14,11 +14,10 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Box from "@mui/material/Box";
 import Radio from '@mui/material/Radio';
 import axios from "axios";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+const BootstrapDialog = styled(Dialog)(({theme}) => ({
     '& .MuiDialogContent-root': {
         padding: theme.spacing(2),
     },
@@ -28,10 +27,10 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 }));
 
 const BootstrapDialogTitle = (props) => {
-    const { children, onClose, ...other } = props;
+    const {children, onClose, ...other} = props;
 
     return (
-        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        <DialogTitle sx={{m: 0, p: 2}} {...other}>
             {children}
             {onClose ? (
                 <IconButton
@@ -44,7 +43,7 @@ const BootstrapDialogTitle = (props) => {
                         color: (theme) => theme.palette.grey[500],
                     }}
                 >
-                    <CloseIcon />
+                    <CloseIcon/>
                 </IconButton>
             ) : null}
         </DialogTitle>
@@ -58,54 +57,59 @@ BootstrapDialogTitle.propTypes = {
 
 
 const AccountDialogs = (props) => {
+    const [data, setData] = useState({})
+    const [switchToggle, setSwitchToggle] = useState(false);
+    useEffect(() => {
+        setData(props.data);
+        setSwitchToggle(data.status === 'open');
+    }, [props.data])
 
-
-    const [data, setData] = useState({
-        type: "",
-        name: "",
-        description: "",
-        quantity: 30,
-        remainder:0,
-        interest: 0,
-        status: ''
-    })
-
-
-    function submit(e) {
+    function handleSubmit(e) {
         e.preventDefault();
-        axios.post('http://localhost:5002/api/admin/accounts',data)
-        .then(res=>{
-            props.close();
-        })
+        axios.post('http://localhost:5002/api/admin/accounts', data)
+            .then(res => {
+                props.close();
+            })
     }
 
+    const handleEdit = (e) => {
+        e.preventDefault();
+        axios.put(`http://localhost:5002/api/admin/accounts/${data._id}`, data)
+            .then(res => {
+                props.close();
+            })
+    }
     function handleChange(e) {
         const newdata = {...data}
         newdata[e.target.name] = e.target.value
         setData(newdata)
     }
 
+    const handleStatusToggle = () => {
+        setSwitchToggle(!switchToggle);
+        setData({...data, status: (!switchToggle)?'open':'close'})
+    }
+
     return (
         <div>
             <BootstrapDialog
-                // onClose={props.close}
                 aria-labelledby="customized-dialog-title"
                 open={props.open}
             >
                 <BootstrapDialogTitle id="customized-dialog-title" onClose={props.close} color='red'>
-                    Add Account
+                    {data['_id'] === undefined ? 'Add Account' : 'Edit account'}
                 </BootstrapDialogTitle>
                 <DialogContent dividers>
-                    <Box component="form" noValidate >
+                    <Box component="form" noValidate>
                         <RadioGroup
                             row
                             aria-labelledby="demo-radio-buttons-group-label"
                             name="type"
                             value={data.type}
-                            onChange={handleChange}                  
+                            onChange={handleChange}
                         >
-                            <FormControlLabel value="saving" control={<Radio />} label="Saving" />
-                            <FormControlLabel value="credit" control={<Radio />} label="Credit" />
+                            <FormControlLabel value="saving" control={<Radio/>} label="Saving"/>
+                            <FormControlLabel value="chequing" control={<Radio/>} label="Credit"/>
                         </RadioGroup>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={12}>
@@ -174,30 +178,38 @@ const AccountDialogs = (props) => {
                             <Grid item xs={12} sm={12}>
                                 <FormControlLabel
                                     control={
-                                        <Switch name="Status" />
+                                        <Switch name="Status"
+                                                checked={switchToggle}
+                                                onClick={handleStatusToggle}
+                                        />
                                     }
                                     labelPlacement="start"
                                     label="Status"
                                     value={data.status}
-                                    sx={{ marginLeft: '0' }}
-                                    onChange={handleChange}
-                                />
+                                    sx={{marginLeft: '0'}}/>
                             </Grid>
                         </Grid>
                     </Box>
                 </DialogContent>
                 <DialogActions>
-                    <Button
+                    {data['_id'] === undefined ? <Button
                         type="submit"
                         variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                        onClick={(e) => submit(e)}>
+                        sx={{mt: 3, mb: 2}}
+                        onClick={handleSubmit}>
                         Submit
-                    </Button>
+                    </Button> : <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{mt: 3, mb: 2}}
+                        onClick={handleEdit}>
+                        Edit
+                    </Button>}
                     <Button
                         type="reset"
                         variant="outlined"
-                        sx={{ mt: 3, mb: 2 }}
+                        sx={{mt: 3, mb: 2}}
+                        onClick={()=>props.close()}
                     >
                         Cancel
                     </Button>
