026afe55508aa2942965c57ba4ef219b87a7fd9a
HANA-BOOK\smile <smilecana@gmail.com>
2022-08-28 23:01:20 -0700

    update proxy middleware file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        createProxyMiddleware(["/api"], { target: "http://localhost:5000" })
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+        })
     );
 };
