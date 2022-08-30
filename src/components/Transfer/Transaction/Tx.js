f8a532c6b49fda7a37e8b38ca4ce93ab43d9b657
yhupaul <yhupaul@hotmail.com>
2022-08-30 12:28:12 -0700

    Tx select

--- src/components/Transfer/Transaction/Tx.js
+++ src/components/Transfer/Transaction/Tx.js
@@ -1,13 +1,15 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import axios from 'axios';
-import { API_TX_POST, getApiRoute } from 'components/commons/module';
+import { API_TX_POST, API_USER_ACCOUNTS_ALL, getApiRoute } from 'components/commons/module';
 import SnackbarAlert from 'components/shared-dialog/SnackbarAlert';
 import PageTitle from 'components/shared-forms/PageTitle';
 import VBButton from 'components/shared-forms/VBButton';
 import VBInputField from 'components/shared-forms/VBInputField';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const initialTx = {
@@ -22,6 +24,9 @@ const initialTx = {
 }
 
 const Transaction = (req, res) => {
+  const params = useParams();
+  const [accounts, setAccounts] = useState([]);
+  const [account, setAccount] = useState({ name: 'test' });
   const [data, setData] = useState(initialTx);
 
   const [error, setError] = useState({
@@ -38,6 +43,11 @@ const Transaction = (req, res) => {
     severity: 'info'
   });
 
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   const createTX = () => {
     axios.post(getApiRoute(API_TX_POST), data).then(res => {
       setAlert({ ...alert, open: true, message: res.data.message, status: res.status });
@@ -46,17 +56,30 @@ const Transaction = (req, res) => {
       console.log(error.toJSON());
     })
   }
+  
+  const getData = () => {
+    axios
+      .get(getApiRoute(API_USER_ACCOUNTS_ALL, { 'userId': params.userId }))
+      .then((res) => {
+        setAccounts(res.data.accounts);
+      })
+      .catch((err) => console.log(err));
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     createTX();
   }
 
+  const handleChange = (event) => {
+    setAccount(event.target.value);
+  };
+
+  const 
+
   //Data that make up page
   const FormFields = {
     schema: [
-      { id: 'from', label: 'From', name: 'from', type: 'default' },
-      { id: 'to', label: 'To', name: 'to', type: 'default' },
       { id: 'amount', label: 'Amount', name: 'amount', type: 'default' },
       { id: 'source', label: 'Source', name: 'source', type: 'default' }
     ]
@@ -74,7 +97,44 @@ const Transaction = (req, res) => {
     <Container component="main" maxWidth="xs">
 
       <PageTitle title="Transaction" />
-
+      <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">From</InputLabel>
+      <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={account}
+          label="From"
+          onChange={handleChange}
+        >
+          {accounts && accounts.map((row, idx) => (
+            <MenuItem
+              key={idx}
+              value={row}
+            >
+              {row.name} ({row.number})
+            </MenuItem>
+          ))}
+        </Select>
+        </FormControl>
+      <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">To</InputLabel>
+      <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={account}
+          label="To"
+          onChange={handleChange}
+        >
+          {accounts && accounts.map((row, idx) => (
+            <MenuItem
+              key={idx}
+              value={row}
+            >
+              {row.name} ({row.number})
+            </MenuItem>
+          ))}
+        </Select>
+        </FormControl>
       <Grid container spacing={2}>
         {FormFields.schema.map((form, idx) => (
 
