579ba0e42b6eb29a90f6e27817e14d986e94f9d7
yhupaul <yhupaul@hotmail.com>
2022-08-30 14:59:01 -0700

    created selection for from and to

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, {useEffect, useState} from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import TransferHistory from "components/Transfer/SendMoney/transferHistory";
+import UserAccounts from "pages/Transaction";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AccountContract from "./components/Account/accountContract/AccountContract";
 import MenuAppBar from "./components/commons/MenuAppBar";
 import PrivateRoute from "./components/commons/PrivateRoute";
-import AccountContract from "./components/Account/accountContract/AccountContract";
+import ContactList from "./components/Contact/ContactList";
 import Transaction from "./components/Transfer/Transaction/Tx";
 import TxHistory from "./components/Transfer/Transaction/TxHistory";
-import UserAccounts from "pages/Transaction";
-import ContactList from "./components/Contact/ContactList";
-import Profile from "./pages/Profile";
-import User from "./pages/admin/User";
-import Account from "./pages/admin/Account";
-import UserAccount from "./pages/UserAccount";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
 import VBStyledCollection from "./components/VBStyledCollection";
+import Account from "./pages/admin/Account";
+import User from "./pages/admin/User";
 import Home from "./pages/Home";
-import TransferHistory from "components/Transfer/SendMoney/transferHistory";
-import UserAccountsTr from "components/Transfer/SendMoney/userAccountList";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import UserAccount from "./pages/UserAccount";
 
 
 const App = () => {
@@ -35,28 +34,28 @@ const App = () => {
     }
     return (
         <>
-            <Box sx={{display: 'flex'}}>
+            <Box sx={{ display: 'flex' }}>
                 <Router>
-                    <CssBaseline/>
+                    <CssBaseline />
                     <MenuAppBar open={open} drawerOpen={handleDrawerOpen} auth={auth} />
                     <VBStyledCollection.Main open={open} auth={auth}>
-                        <VBStyledCollection.DrawerHeader/>
+                        <VBStyledCollection.DrawerHeader />
                         <Routes>
-                            <Route path="/" element={<Home/>}/>
-                            <Route path="/user/:userId" element={<PrivateRoute auth={auth} children={<Profile/>}/>}/>
-                            <Route path="/user/:userId/accounts" element={<PrivateRoute auth={auth} children={<UserAccount/>}/>}/>
-                            <Route path="/user/:userId/accounts/:accountId" exact element={<PrivateRoute auth={auth} children={<Home/>}/>}/>
-                            <Route path="/user/:userId/contacts" element={<PrivateRoute auth={auth} children={<ContactList/>}/>}/>
-                            <Route path="/accounts/:accountId/open" exact element={<PrivateRoute auth={auth} children={<AccountContract auth={auth}/>}/>}/>
-                            <Route path="/admin/users" exact element={<PrivateRoute auth={auth} children={<User/>}/>}/>
-                            <Route path="/admin/accounts" element={<PrivateRoute auth={auth} children={<Account/>}/>}/>
-                            <Route path="/login" element={<Login/>} />
-                            <Route path="/user/:userId/tx" exact element={<PrivateRoute auth={auth} children={<Transaction/>}/>}/>
-                            <Route path="/tx/:userAccountId" element={<PrivateRoute auth={auth} children={<TxHistory/>}/>}/>
-                            <Route path="/user/:userId/txs" element={<PrivateRoute auth={auth} children={<UserAccounts/>}/>}/>
-                            <Route path="/signup" element={<SignUp/>}/>
-                            <Route path="/transfer/:userAccountId" element={<PrivateRoute auth={auth} children={<TransferHistory/>}/>}/>
-                            <Route path="/user/:userId/transfer" element={<PrivateRoute auth={auth} children={<UserAccount/>}/>}/>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/user/:userId" element={<PrivateRoute auth={auth} children={<Profile />} />} />
+                            <Route path="/user/:userId/accounts" element={<PrivateRoute auth={auth} children={<UserAccount />} />} />
+                            <Route path="/user/:userId/accounts/:accountId" exact element={<PrivateRoute auth={auth} children={<Home />} />} />
+                            <Route path="/user/:userId/contacts" element={<PrivateRoute auth={auth} children={<ContactList />} />} />
+                            <Route path="/accounts/:accountId/open" exact element={<PrivateRoute auth={auth} children={<AccountContract auth={auth} />} />} />
+                            <Route path="/admin/users" exact element={<PrivateRoute auth={auth} children={<User />} />} />
+                            <Route path="/admin/accounts" element={<PrivateRoute auth={auth} children={<Account />} />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/user/:userId/tx" exact element={<PrivateRoute auth={auth} children={<Transaction />} />} />
+                            <Route path="/tx/:userAccountId" element={<PrivateRoute auth={auth} children={<TxHistory />} />} />
+                            <Route path="/user/:userId/txs" element={<PrivateRoute auth={auth} children={<UserAccounts />} />} />
+                            <Route path="/signup" element={<SignUp />} />
+                            <Route path="/transfer/:userAccountId" element={<PrivateRoute auth={auth} children={<TransferHistory />} />} />
+                            <Route path="/user/:userId/transfer" element={<PrivateRoute auth={auth} children={<UserAccount />} />} />
                         </Routes>
                     </VBStyledCollection.Main>
                 </Router>
--- src/components/Transfer/Transaction/TxHistory.js
+++ src/components/Transfer/Transaction/TxHistory.js
@@ -1,20 +1,20 @@
-import * as React from 'react';
-import {useEffect, useState} from 'react';
 import Container from '@mui/material/Container';
 import Typography from "@mui/material/Typography";
 import axios from "axios";
-import { useParams } from 'react-router-dom';
 import { API_USER_TX, getApiRoute } from 'components/commons/module';
-import PageTitle from 'components/shared-forms/PageTitle';
 import DynamicTable from 'components/shared-forms/DynamicTable';
+import PageTitle from 'components/shared-forms/PageTitle';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const initialTx = {
-    from: '',
-    to: '',
-    amount: '',
-    source: '',
-    type: '',
+  from: '',
+  to: '',
+  amount: '',
+  source: '',
+  type: '',
 }
 
 const TxHistory = (props) => {
@@ -29,35 +29,35 @@ const TxHistory = (props) => {
   //${props.data._id}
 
   const getData = () => {
-    axios.get(getApiRoute(API_USER_TX, {'userAccountId': props.data._id}))
-        .then((res) => {
-          console.log(res);
-          setTxs(res.data.userAccount.transactions); 
-        }).catch(e => {
-          console.log(e);
-        });
-      }
+    axios.get(getApiRoute(API_USER_TX, { 'userAccountId': props.data._id }))
+      .then((res) => {
+        console.log(res);
+        setTxs(res.data.userAccount.transactions);
+      }).catch(e => {
+        console.log(e);
+      });
+  }
 
-      const UserTxData = {
-        schema: [
-            {head: 'Id', cols: '_id', format: 'default'},
-            {head: 'From', cols: 'from', format: 'default'},
-            {head: 'To', cols: '_id' , format: 'default'},
-            {head: 'Amount', cols: 'amount', format: 'default'},
-            {head: 'Source', cols: 'source', format: 'default'},
-        ]
-    }
+  const UserTxData = {
+    schema: [
+      { head: 'Id', cols: '_id', format: 'default' },
+      { head: 'From', cols: 'from', format: 'default' },
+      { head: 'To', cols: '_id', format: 'default' },
+      { head: 'Amount', cols: 'amount', format: 'default' },
+      { head: 'Source', cols: 'source', format: 'default' },
+    ]
+  }
 
   return (
     <Container component="main">
-      <PageTitle title="Transaction History"/>
+      <PageTitle title="Transaction History" />
       {txs.length !== 0 ?
-      <DynamicTable form={UserTxData} data={txs}/>
-                :
-                <Typography variant="h5" gutterBottom component="div" mt={2}>
-                No data
-            </Typography>
-        }
+        <DynamicTable form={UserTxData} data={txs} />
+        :
+        <Typography variant="h5" gutterBottom component="div" mt={2}>
+          No data
+        </Typography>
+      }
     </Container>
   );
 };
