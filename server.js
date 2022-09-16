53d002d208f927ec4b7c8cd75b00dc25143d440a
HANA-BOOK\smile <smilecana@gmail.com>
2022-09-16 15:30:13 -0700

    FEATURE: heroku build

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const routes = require('./routes');
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 const corsOptions ={
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 }
 const app = express();
-
 app.use(cookieParser());
-app.use(cors(corsOptions))
+app.options('*', cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());  //express.json + express.urlencoded()bodyParser
 
