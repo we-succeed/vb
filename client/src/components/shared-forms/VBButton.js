362482e58d6a01d2dadd0f40eb06efc7b392d99e
yhupaul <yhupaul@hotmail.com>
2022-09-19 15:46:18 -0700

    changed button color and hover color

--- client/src/components/shared-forms/VBButton.js
+++ client/src/components/shared-forms/VBButton.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import Button from "@mui/material/Button";
 
+
 const VBButton = (props) => {
     return (
         <>
             <Button
                 fullWidth={(props.fullWidth)}
                 variant="contained"
-                sx={{mt: 3, mb: 2}}
-                onClick={props.onClick}
+                sx={{
+                    mt: 3,
+                    mb: 2,
+                    background:'#2e3b55',
+                    '&:hover': {
+                        background: '#2b41cd'
+                    }
+                    }}
+                onClick={props.onClick}               
             >
                 {props.title}
             </Button>
